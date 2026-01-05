535dd91780a320eefb86bedfbe8972ce3ee3bce3
mo.yadavi <mo.yadavi@MO-YADAVI>
2026-01-05 11:22:42 +0330

    making Logo 360 degree rotate

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,9 +5,9 @@ const Loading: React.FC = () => {
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-white dark:bg-navy-950 z-50">
       {/* لوگوی چرخشی */}
       <img
-        src="/images/Logo.png" // مسیر درست public
+        src="/images/LoadingLogo.png" // مسیر درست public
         alt="Logo"
-        className="h-32 w-32 animate-bounce"
+       className="h-32 w-32 animate-logo-rotate"
       />
     </div>
   );
