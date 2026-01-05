97ef1977500064e4fb5ec3d4dcbfcbb596abea0f
mo.yadavi <mo.yadavi@MO-YADAVI>
2026-01-05 16:14:13 +0330

    fixing the animation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,7 +5,7 @@ const Loading: React.FC = () => {
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-white dark:bg-navy-950 z-50">
       {/* لوگوی چرخشی */}
       <img
-        src="/images/LoadingLogo.png" // مسیر درست public
+        src="/images/Logo.png" // مسیر درست public
         alt="Logo"
        className="h-32 w-32 animate-logo-rotate"
       />
